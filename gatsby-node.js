506f53b528aa6763f71767905f94f35f1a3637f3
John Hamson <jhamson@fuzzproductions.com>
2021-01-05 17:56:44 -0600

    gatsby node onCreatePage?

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,34 +1,48 @@
 const path = require("path");
 
+// pages folder
+exports.onCreatePage = ({ page, actions }) => {
+  const { createPage, deletePage } = actions;
+  deletePage(page);
+  // You can access the variable "locale" in your page queries now
+  createPage({
+    ...page,
+    context: {
+      ...page.context,
+      locale: page.context.intl.language,
+    },
+  });
+};
+
 exports.createPages = ({ graphql, actions }) => {
-    const { createPage } = actions;
-    const layoutTemplate = path.resolve(`src/templates/pageTemplate.js`);
-    return graphql(`
+  const { createPage } = actions;
+  const layoutTemplate = path.resolve(`src/templates/pageTemplate.js`);
+  return graphql(`
     query {
-        allContentfulLayout {
-            edges {
-                node {
-                    slug
-                }
-            }
+      allContentfulLayout {
+        edges {
+          node {
+            slug
+          }
         }
+      }
     }
   `).then(result => {
-        if (result.errors) {
-            throw result.errors;
-        }
-        result.data.allContentfulLayout.edges.forEach(edge => {
-            if (edge.node.slug === "404") {
-                // for 404 page we use custom page at src/pages/404.js
-                return;
-            }
-            createPage({
-                path: edge.node.slug,
-                component: layoutTemplate,
-                context: {
-                    slug: edge.node.slug
-                }
-            });
-        });
+    if (result.errors) {
+      throw result.errors;
+    }
+    result.data.allContentfulLayout.edges.forEach(edge => {
+      if (edge.node.slug === "404") {
+        // for 404 page we use custom page at src/pages/404.js
+        return;
+      }
+      createPage({
+        path: edge.node.slug,
+        component: layoutTemplate,
+        context: {
+          slug: edge.node.slug,
+        },
+      });
     });
+  });
 };
