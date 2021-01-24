2c0589031b2cf6cdd9b65b6d2d29935131534f3c
John Hamson <jhamson@fuzzproductions.com>
2021-01-24 14:44:06 -0600

    gatsby-source-contentful-locales doesn't have a env

--- gatsby-config.js
+++ gatsby-config.js
@@ -54,8 +54,7 @@ module.exports = {
       resolve: `gatsby-source-contentful-locales`,
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
-        environment: process.env.CONTENTFUL_ENVIRONMENT
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
       },
     },
     `@contentful/gatsby-transformer-contentful-richtext`
